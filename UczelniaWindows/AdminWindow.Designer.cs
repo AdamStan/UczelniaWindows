557e88e00000a1a2c26cffa0ffe7383fd5aaf3c7
AdamStan <adamstanczyk96@gmail.com>
2018-05-24 23:19:18 +0200

    DataGridView in AdminWindow works, but it needs futher improvments

--- UczelniaWindows/AdminWindow.Designer.cs
+++ UczelniaWindows/AdminWindow.Designer.cs
@@ -28,34 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.buttonLogout = new System.Windows.Forms.Button();
             this.buttonLoad = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.uczelniaDataSet = new UczelniaWindows.UczelniaDataSet();
-            this.administratorsBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.administratorsTableAdapter = new UczelniaWindows.UczelniaDataSetTableAdapters.AdministratorsTableAdapter();
-            this.tableAdapterManager = new UczelniaWindows.UczelniaDataSetTableAdapters.TableAdapterManager();
-            this.facultiesBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.facultiesTableAdapter = new UczelniaWindows.UczelniaDataSetTableAdapters.FacultiesTableAdapter();
-            this.marksBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.marksTableAdapter = new UczelniaWindows.UczelniaDataSetTableAdapters.MarksTableAdapter();
-            this.studentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.studentsTableAdapter = new UczelniaWindows.UczelniaDataSetTableAdapters.StudentsTableAdapter();
-            this.subjectsBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.subjectsTableAdapter = new UczelniaWindows.UczelniaDataSetTableAdapters.SubjectsTableAdapter();
-            this.tutorsBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.tutorsTableAdapter = new UczelniaWindows.UczelniaDataSetTableAdapters.TutorsTableAdapter();
             this.ConfirmButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.uczelniaDataSet)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.administratorsBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.facultiesBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.marksBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.studentsBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.subjectsBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tutorsBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonLogout
@@ -94,84 +72,6 @@
             this.comboBox1.TabIndex = 2;
             this.comboBox1.Text = "(wybierz tabele)";
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 42);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(693, 259);
-            this.dataGridView1.TabIndex = 3;
-            // 
-            // uczelniaDataSet
-            // 
-            this.uczelniaDataSet.DataSetName = "UczelniaDataSet";
-            this.uczelniaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // administratorsBindingSource
-            // 
-            this.administratorsBindingSource.DataMember = "Administrators";
-            this.administratorsBindingSource.DataSource = this.uczelniaDataSet;
-            // 
-            // administratorsTableAdapter
-            // 
-            this.administratorsTableAdapter.ClearBeforeFill = true;
-            // 
-            // tableAdapterManager
-            // 
-            this.tableAdapterManager.AdministratorsTableAdapter = this.administratorsTableAdapter;
-            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
-            this.tableAdapterManager.FacultiesTableAdapter = this.facultiesTableAdapter;
-            this.tableAdapterManager.MarksTableAdapter = this.marksTableAdapter;
-            this.tableAdapterManager.StudentsTableAdapter = this.studentsTableAdapter;
-            this.tableAdapterManager.SubjectsTableAdapter = this.subjectsTableAdapter;
-            this.tableAdapterManager.TutorsTableAdapter = this.tutorsTableAdapter;
-            this.tableAdapterManager.UpdateOrder = UczelniaWindows.UczelniaDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
-            // 
-            // facultiesBindingSource
-            // 
-            this.facultiesBindingSource.DataMember = "Faculties";
-            this.facultiesBindingSource.DataSource = this.uczelniaDataSet;
-            // 
-            // facultiesTableAdapter
-            // 
-            this.facultiesTableAdapter.ClearBeforeFill = true;
-            // 
-            // marksBindingSource
-            // 
-            this.marksBindingSource.DataMember = "Marks";
-            this.marksBindingSource.DataSource = this.uczelniaDataSet;
-            // 
-            // marksTableAdapter
-            // 
-            this.marksTableAdapter.ClearBeforeFill = true;
-            // 
-            // studentsBindingSource
-            // 
-            this.studentsBindingSource.DataMember = "Students";
-            this.studentsBindingSource.DataSource = this.uczelniaDataSet;
-            // 
-            // studentsTableAdapter
-            // 
-            this.studentsTableAdapter.ClearBeforeFill = true;
-            // 
-            // subjectsBindingSource
-            // 
-            this.subjectsBindingSource.DataMember = "Subjects";
-            this.subjectsBindingSource.DataSource = this.uczelniaDataSet;
-            // 
-            // subjectsTableAdapter
-            // 
-            this.subjectsTableAdapter.ClearBeforeFill = true;
-            // 
-            // tutorsBindingSource
-            // 
-            this.tutorsBindingSource.DataMember = "Tutors";
-            this.tutorsBindingSource.DataSource = this.uczelniaDataSet;
-            // 
-            // tutorsTableAdapter
-            // 
-            this.tutorsTableAdapter.ClearBeforeFill = true;
-            // 
             // ConfirmButton
             // 
             this.ConfirmButton.Location = new System.Drawing.Point(221, 14);
@@ -182,13 +82,21 @@
             this.ConfirmButton.UseVisualStyleBackColor = true;
             this.ConfirmButton.Click += new System.EventHandler(this.Confirm);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(693, 259);
+            this.dataGridView1.TabIndex = 5;
+            // 
             // AdminWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(726, 313);
-            this.Controls.Add(this.ConfirmButton);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ConfirmButton);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.buttonLoad);
             this.Controls.Add(this.buttonLogout);
@@ -196,13 +104,6 @@
             this.Text = "AdminWindow";
             this.Load += new System.EventHandler(this.AdminWindow_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.uczelniaDataSet)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.administratorsBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.facultiesBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.marksBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.studentsBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.subjectsBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tutorsBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -212,21 +113,7 @@
         private System.Windows.Forms.Button buttonLogout;
         private System.Windows.Forms.Button buttonLoad;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private UczelniaDataSet uczelniaDataSet;
-        private System.Windows.Forms.BindingSource administratorsBindingSource;
-        private UczelniaDataSetTableAdapters.AdministratorsTableAdapter administratorsTableAdapter;
-        private UczelniaDataSetTableAdapters.TableAdapterManager tableAdapterManager;
-        private UczelniaDataSetTableAdapters.FacultiesTableAdapter facultiesTableAdapter;
-        private System.Windows.Forms.BindingSource facultiesBindingSource;
-        private UczelniaDataSetTableAdapters.MarksTableAdapter marksTableAdapter;
-        private System.Windows.Forms.BindingSource marksBindingSource;
-        private UczelniaDataSetTableAdapters.StudentsTableAdapter studentsTableAdapter;
-        private System.Windows.Forms.BindingSource studentsBindingSource;
-        private UczelniaDataSetTableAdapters.SubjectsTableAdapter subjectsTableAdapter;
-        private System.Windows.Forms.BindingSource subjectsBindingSource;
-        private UczelniaDataSetTableAdapters.TutorsTableAdapter tutorsTableAdapter;
-        private System.Windows.Forms.BindingSource tutorsBindingSource;
         private System.Windows.Forms.Button ConfirmButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- UczelniaWindows/AdminWindow.cs
+++ UczelniaWindows/AdminWindow.cs
@@ -14,7 +14,8 @@ namespace UczelniaWindows
     {
         private Form formBehind;
         //DataClasses1DataContext dc = new DataClasses1DataContext();
-        private BindingSource bs_currently;
+        UczelniaEntities context;
+
         public AdminWindow(Form behind)
         {
             this.formBehind = behind;
@@ -29,55 +30,47 @@ namespace UczelniaWindows
 
         private void LoadTableToGrid(object sender, EventArgs e)
         {
-            dataGridView1.AutoGenerateColumns = true;
             if (comboBox1.Text == "(wybierz tabele)")
             {
                 MessageBox.Show("Nie wybrano tabeli");
             }
             else
             {
+                context = new UczelniaEntities();
                 if (comboBox1.Text == "tutors")
                 {
-                    this.dataGridView1.DataSource = this.tutorsBindingSource;
-                    this.bs_currently = this.tutorsBindingSource;
+                    dataGridView1.DataSource = context.Tutors.ToList();
                 }
                 else if (comboBox1.Text == "administrators")
                 {
-                    this.dataGridView1.DataSource = this.administratorsBindingSource;
-                    this.bs_currently = this.administratorsBindingSource;
+                    dataGridView1.DataSource = context.Administrators.ToList();
                 }
                 else if (comboBox1.Text == "students")
-                    this.dataGridView1.DataSource = this.studentsBindingSource;
+                {
+                    dataGridView1.DataSource = context.Students.ToList();
+                }
                 else if (comboBox1.Text == "marks")
-                    this.dataGridView1.DataSource = this.marksBindingSource;
+                {
+                    dataGridView1.DataSource = context.Marks.ToList();
+                }
                 else if (comboBox1.Text == "subjects")
-                    this.dataGridView1.DataSource = this.subjectsBindingSource;
+                {
+                    dataGridView1.DataSource = context.Students.ToList();
+                }
                 else if (comboBox1.Text == "faculties")
-                    this.dataGridView1.DataSource = this.facultiesBindingSource;
+                {
+                    dataGridView1.DataSource = context.Faculties.ToList();
+                }
             }
         }
 
         private void AdminWindow_Load(object sender, EventArgs e)
         {
-            // TODO: Ten wiersz kodu wczytuje dane do tabeli 'uczelniaDataSet.Tutors' . Możesz go przenieść lub usunąć.
-            this.tutorsTableAdapter.Fill(this.uczelniaDataSet.Tutors);
-            // TODO: Ten wiersz kodu wczytuje dane do tabeli 'uczelniaDataSet.Subjects' . Możesz go przenieść lub usunąć.
-            this.subjectsTableAdapter.Fill(this.uczelniaDataSet.Subjects);
-            // TODO: Ten wiersz kodu wczytuje dane do tabeli 'uczelniaDataSet.Students' . Możesz go przenieść lub usunąć.
-            this.studentsTableAdapter.Fill(this.uczelniaDataSet.Students);
-            // TODO: Ten wiersz kodu wczytuje dane do tabeli 'uczelniaDataSet.Marks' . Możesz go przenieść lub usunąć.
-            this.marksTableAdapter.Fill(this.uczelniaDataSet.Marks);
-            // TODO: Ten wiersz kodu wczytuje dane do tabeli 'uczelniaDataSet.Faculties' . Możesz go przenieść lub usunąć.
-            this.facultiesTableAdapter.Fill(this.uczelniaDataSet.Faculties);
-            // TODO: Ten wiersz kodu wczytuje dane do tabeli 'uczelniaDataSet.Administrators' . Możesz go przenieść lub usunąć.
-            this.administratorsTableAdapter.Fill(this.uczelniaDataSet.Administrators);
 
         }
-
         private void Confirm(object sender, EventArgs e)
         {
-            this.bs_currently.EndEdit();
-            
+            context.SaveChanges();
         }
     }
 }
